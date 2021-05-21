bae6e915a5a0a516ea8fb4ec88e91ebfca935070
Trustedinstall <68763785+Trustedinstall@users.noreply.github.com>
2021-05-21 19:47:13 +0800

    Add files via upload

--- dostool.js
+++ dostool.js
@@ -68,7 +68,7 @@ for /f "delims=" %%a in ('hostname') do set hostname=%%a
 cd/d "%disk%\"
 set cishu=3
 set ver=20210521
-set versize=166724
+set versize=166726
 set gxflag=
 set baidu=start https://www.baidu.com/s?wd=
 set google=start https://www.google.com.hk/search?q=
@@ -4000,10 +4000,10 @@ set xzflag=::
 set xzflag1=
 if exist %systemroot%\system32\curl.exe (set xzflag1=::&set xzflag=)
 %xzflag%pushd %temp%
-%xzflag%curl -# -O https://cdn.jsdelivr.net/gh/Trustedinstall/dostool/dostool.js
+%xzflag%curl -# -o dostool https://cdn.jsdelivr.net/gh/Trustedinstall/dostool/dostool.js
 %xzflag%popd
-%xzflag%call :hash %temp%\dostool.js sha1
-%xzflag%if /i "%hash%" equ %doshash% copy /z /y %temp%\dostool.js %weizhi%&start cmd /c %0&exit
+%xzflag%call :hash %temp%\dostool sha1
+%xzflag%if /i "%hash%" equ %doshash% copy /z /y %temp%\dostool %weizhi%&start cmd /c %0&exit
 %xzflag1%certutil -urlcache -split -f https://cdn.jsdelivr.net/gh/Trustedinstall/dostool/dostool.js %temp%\dostool
 %xzflag1%call :hash %temp%\dostool sha1
 %xzflag1%if /i "%hash%" equ %doshash% copy /z /y %temp%\dostool %weizhi%&start cmd /c %0&exit
